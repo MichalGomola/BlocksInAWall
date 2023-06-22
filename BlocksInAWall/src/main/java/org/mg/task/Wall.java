d96180251990ce0e223f63c12b3c171f50054884
MichalGomola <michal.gomola.89@gmail.com>
2023-06-22 19:55:01 +0200

    anti null update

--- BlocksInAWall/src/main/java/org/mg/task/Wall.java
+++ BlocksInAWall/src/main/java/org/mg/task/Wall.java
@@ -1,15 +1,20 @@
 package org.mg.task;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.Objects.isNull;
+import static org.apache.commons.collections4.CollectionUtils.emptyIfNull;
 
 public class Wall implements Structure{
     private List<Block> blocks;
 
     @Override
     public Optional<Block> findBlockByColor(String color) {
+        if(isNull(color)) {return Optional.empty()}
         return prepareAllBlocks()
                 .filter(b -> b.getColor().equals(color))
                 .findAny();
@@ -17,6 +22,7 @@ public class Wall implements Structure{
 
     @Override
     public List<Block> findBlocksByMaterial(String material) {
+        if (isNull(material)) {return new ArrayList<>();}
         return prepareAllBlocks().filter(b -> b.getMaterial().equals(material))
                 .collect(Collectors.toList());
     }
@@ -27,7 +33,7 @@ public class Wall implements Structure{
     }
 
     private Stream<Block> prepareAllBlocks() {
-        return blocks.stream().
+        return emptyIfNull(blocks).stream().
                 flatMap(b -> {
                     if (b instanceof CompositeBlock) {
                         return Stream.concat(Stream.of(b), prepareAllBlocks(((CompositeBlock) b).getBlocks())) ;
@@ -38,7 +44,7 @@ public class Wall implements Structure{
     }
 
     private Stream<Block> prepareAllBlocks(List<Block> blocks) {
-        return blocks.stream().
+        return emptyIfNull(blocks).stream().
                 flatMap(b -> {
                     if (b instanceof CompositeBlock) {
                         return Stream.concat(Stream.of(b), prepareAllBlocks(((CompositeBlock) b).getBlocks()));
